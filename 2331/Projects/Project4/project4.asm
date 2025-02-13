256a0c45f34fe160de8ec306cf383f85895b1236
rifatabrarjowad <rajdiprifat2004@gmail.com>
2025-02-13 16:42:35 -0600

    cosc31 inclass

--- 2331/Projects/Project4/project4.asm
+++ 2331/Projects/Project4/project4.asm
@@ -1,25 +1,63 @@
-; Purpose: The implementation of Project 4
-
 %include "asm_io.inc"
 
-; initialized data
 segment .data
+    prompt db "Enter your favorite number: ", 0
+    newline db 10, 0
+    times10_msg db "Number * 10: ", 0
+    times72_msg db "Number * 72: ", 0
+    times81_msg db "Number * 81: ", 0
 
-; uninitialized data
 segment .bss
+    number resd 1  ; Reserve space for an integer
 
 segment .text
-        global  asm_main
+    global asm_main
 asm_main:
-        enter   0,0               ; setup routine
-        pusha
+    enter 0, 0
+    pusha
 
-        ; NO EXPECTED OUTPUT (YET)
-        ; TODO: Implemement project 4
+    ; Print prompt
+    mov eax, prompt
+    call print_string
 
-        popa
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
+    ; Read integer input
+    call read_int
+    mov [number], eax  ; Store input in variable
 
+    ; Print newline
+    mov eax, newline
+    call print_string
 
+    ; Print starting number
+    mov eax, [number]
+    call print_int
+    call print_nl
+
+    ; Calculate and print number * 10
+    mov eax, times10_msg
+    call print_string
+    mov eax, [number]
+    imul eax, 10
+    call print_int
+    call print_nl
+
+    ; Calculate and print number * 72
+    mov eax, times72_msg
+    call print_string
+    mov eax, [number]
+    imul eax, 72
+    call print_int
+    call print_nl
+
+    ; Calculate and print number * 81
+    mov eax, times81_msg
+    call print_string
+    mov eax, [number]
+    imul eax, 81
+    call print_int
+    call print_nl
+
+    popa
+    mov eax, 0  ; Return 0
+    leave
+    ret
--- 2331/Unit4_Assembly/Part3_Shift_and_Bitwise_Operations/shifts_and_bitwise_notes.asm
+++ 2331/Unit4_Assembly/Part3_Shift_and_Bitwise_Operations/shifts_and_bitwise_notes.asm
@@ -16,6 +16,55 @@ asm_main:
         pusha
 
         ; NO EXPECTED OUTPUT (YET)
+        mov eax, 0FH ;
+        shl eax, 4 
+        dump_regs 1
+        call print_nl
+
+        mov eax, 080000000H
+        shr eax, 5
+        dump_regs 2
+        call print_nl
+
+        mov eax, 0F0000000H
+        shl eax, 5
+        mov ebx, 0
+        setc bl
+        dump_regs 3
+        call print_nl
+
+        mov eax, 080000000H
+        sar eax, 3
+        dump_regs 4
+        call print_nl
+        
+        mov eax, 080000000H
+        rol eax, 3
+        dump_regs 5
+        call print_nl
+
+
+        mov eax, 0FH
+        mov ebx, 09H
+        and eax, ebx
+        dump_regs 6
+        call print_nl
+
+        mov eax, 00BH
+        mov ebx, 04H
+        or eax, ebx 
+        dump_regs 7
+        call print_nl
+
+        mov eax, 0FH
+        mov ebx, 8
+        not ebx
+        and eax, ebx 
+        dump_regs 8
+        call print_nl
+
+        xor eax, eax 
+
 
         popa
         mov     eax, 0            ; return back to C
